3de57d91fcd038b3b0602460d483b213ef07368b
Suhaib Saeed <saeed.suhaib@googlemail.com>
2020-12-23 23:52:02 +0000

    Completed up to task 12

--- source.sh
+++ source.sh
@@ -15,10 +15,30 @@ echo "You are building version"  $version
 echo "Would you like to continue the script?"
 # Put users option into versioncontinue variable
 read -p "1 = yes & 0 = no: " versioncontinue
+
 # Print confirmation to console depending on user input
 if [ $versioncontinue == 1 ]
 then
   echo "OK - continuing script"
+  # Loop through files in the source directory and print filename
+  for filename in source/*
+  do # Do these commands in loop
+    # Inform user below file is not being copied but others are
+    if [ $filename == "source/secretinfo.md" ]
+    then
+      echo $filename "is not being copied"
+    else
+      echo $filename "is being copied"
+      # Copy the file to the build directory
+      cp $filename build/.
+    fi
+  done
+
+  # List the files in the build directory
+  cd build # Change directory first
+  echo "The files in build version $version are:"
+  ls
+
 else
   echo "Please come back when you are ready"
 fi
